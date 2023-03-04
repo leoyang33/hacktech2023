3b51a864b36c30923322c11bbc74d3ef0ecba4a4
vivianz <vlzhang@caltech.edu>
2023-03-04 12:59:30 -0800

    global impact widgets

--- material-kit-react/src/pages/global.js
+++ material-kit-react/src/pages/global.js
@@ -30,8 +30,8 @@ const Page = () => (
           <Grid xs={4}
           padding={1}>
             <GlobalStat
-              text={"Users in your state saved 200,000 kWh of electricity this month!"}
-              title="State impact"
+              text={"Users in California used a combined 200,000 kWh less this month than average. That's 163,600 lbs of carbon out of the atmosphere! Keep up the good work!"}
+              title="Electric savings"
               type="Electricity"
               sx={{ height: '100%' }}
             />
@@ -39,8 +39,8 @@ const Page = () => (
           <Grid xs={4}
           padding={1}>
             <GlobalStat
-              text={"Users in your state saved 50,000 gallons of water this month!"}
-              title="State impact"
+              text={"Users in your state saved 1,000,000 gallons of water total this month. That's a 1% decrease, saving 380,000 lbs of CO2 from wastewater treatment plants!"}
+              title="Water Savings"
               type="Water"
               sx={{ height: '100%' }}
             />
@@ -48,8 +48,8 @@ const Page = () => (
           <Grid xs={4}
           padding={1}>
             <GlobalStat
-              text={"Users in your state saved 200,000 cubic feet of gas this month!"}
-              title="State impact"
+              text={"Users in your state used a combined 200,000 cubic feet less of gas this month. That's 29,600,000 lbs of CO2 out of the atmosphere!"}
+              title="Gas savings"
               type="Gas"
               sx={{ height: '100%' }}
             />
