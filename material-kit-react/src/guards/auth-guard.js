c76f09008da5cc94d508fb76366754f195130233
Leo Yang <leoyang33@yahoo.com>
2023-03-03 23:09:20 -0800

    remove login and top nav

--- material-kit-react/src/guards/auth-guard.js
+++ material-kit-react/src/guards/auth-guard.js
@@ -31,7 +31,7 @@ export const AuthGuard = (props) => {
         console.log('Not authenticated, redirecting');
         router
           .replace({
-            pathname: '/auth/login',
+            pathname: '/',
             query: router.asPath !== '/' ? { continueUrl: router.asPath } : undefined
           })
           .catch(console.error);
